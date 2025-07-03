cfb0908a77e0436afc8e34a2d770940b3be1f793
Arvindsingh Deora <arvindsinghdeora47@gmail.com>
2025-07-03 20:59:19 +0530

    Change pdf resume

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -90,7 +90,7 @@ const Home = () => {
             ))}
           </div>
           <div className="cta-buttons">
-    <a href="/Arvind_SoftwareDeveloper.pdf" download className="resume-btn">Download Resume</a>
+    <a href="/Arvind_SDE_Resume.pdf.pdf" download className="resume-btn">Download Resume</a>
     <a href="/About" className="contact-btn">Contact Me</a>
 </div>
 
